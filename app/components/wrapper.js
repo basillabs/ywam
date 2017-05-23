4375891ee6ca45d2fb8a86f789d1d2f7d7bcc14a
Sang Tian <sangsta@dropbox.com>
2017-05-22 21:40:31 -0700

    Added a hook for Android back button.

--- app/components/wrapper.js
+++ app/components/wrapper.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import {
   AppRegistry,
+  BackHandler,
   StyleSheet,
   Text,
   WebView
@@ -13,11 +14,23 @@ const defaultProps = {
   url: 'https://findmymission.com/',
 };
 
+const WEBVIEW_REF = 'webview';
+
 
 export default class Wrapper extends Component {
+
+  constructor(props) {
+    super();
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      this.refs[WEBVIEW_REF].goBack();
+      return true;
+    });
+  }
+
   render() {
     return (
       <WebView
+        ref={WEBVIEW_REF}
         source={{uri: this.props.url}}
         startInLoadingState
       />
